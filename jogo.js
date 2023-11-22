8ef00ecb367be4d24563628accd1ede883e83b31
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-11-22 14:54:30 -0300

    definindo a dimensão do palco do jogo

---
+++ jogo.js
@@ -0,0 +1,15 @@
+
+
+
+var altura=0;
+var largura=0;
+function ajustarTamanhoPalco(){
+	var altura= window.innerHeight;
+	var largura= window.innerWidth;
+	console.log(altura+' '+largura);
+
+	
+
+}
+ajustarTamanhoPalco();
+	
