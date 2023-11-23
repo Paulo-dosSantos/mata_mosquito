b12e5eab39d76ea1fe957f306c771f31c39ab5e2
Paulo Henrique <paulo.13.henrique800@gmail.com>
2023-11-23 14:47:36 -0300

    corrigindo erro de posição

--- jogo.js
+++ jogo.js
@@ -13,13 +13,14 @@ function ajustarTamanhoPalco(){
 }
 ajustarTamanhoPalco();
  
-var posicaoX =Math.floor( Math.random() * largura)-90;
-var posicaoY = Math.floor(Math.random() *altura )-90;
+function posicaoRandomica(){
+	var posicaoX =Math.floor( Math.random() * largura) -90;
+var posicaoY = Math.floor(Math.random() *altura ) -90;
 
 console.log(posicaoX,posicaoY);
 
 posicaoX= posicaoX<0 ? 0 : posicaoX;
-posicaoY= posicaoX<0 ? 0 : posicaoY;
+posicaoY= posicaoY<0 ? 0 : posicaoY;
 
 
 
@@ -27,12 +28,13 @@ posicaoY= posicaoX<0 ? 0 : posicaoY;
 //criando o elemento html
 var mosquito=  document.createElement('img');
 mosquito.src= 'imagens/mosquito.png';
-mosquito.className=tamanhoAleatorio();
+mosquito.className=tamanhoAleatorio() +' '+ ladoAleatorio();
 mosquito.style.left=posicaoX+'px';
 mosquito.style.top=posicaoY+'px';
 mosquito.style.position='absolute';
 document.body.appendChild(mosquito)
 
+}
 
 function tamanhoAleatorio(){
 	var classe= Math.floor( Math.random()*3);
@@ -50,4 +52,21 @@ function tamanhoAleatorio(){
         return 'mosquito3';       
 
 	} 
+}
+function ladoAleatorio(){
+
+	var classe= Math.floor( Math.random()*2);
+
+	switch(classe){
+
+      case 0:
+        return 'ladoA'; 
+        
+       case 1:
+        return 'ladoB'; 
+
+
+     
+	} 
+
 }
